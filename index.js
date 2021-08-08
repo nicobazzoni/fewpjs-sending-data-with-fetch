8a3c35e03f0ea0972cda2031147128ee9a35e3da
nicobazzoni <jillianamagsila@Jillians-iMac.fios-router.home>
2021-08-08 19:46:33 -0400

    commit tree ddfabc424570c1b581111cdbb65327fc101dfd03

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+function submitData(name, email) {
+    let dogData = {
+        name: name,
+        email: email
+    };
+
+    let apiObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(dogData)
+    };
+
+    return fetch("http://localhost:3000/users", apiObj)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(object) {
+            let h2 = document.createElement('h2');
+            h2.innerHTML = object.id;
+            document.body.appendChild(h2);
+            console.log(object);
+        })
+        .catch(function(error) {
+            let h3 = document.createElement('h3');
+            h3.innerHTML = error.message;
+            document.body.appendChild(h3);
+            console.log(error.message);
+        });
+} 
